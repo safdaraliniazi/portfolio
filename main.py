91c5ac1344eb6b79178f9f442e213d1da8d2d2e3
safdaraliniazi <65447198+safdaraliniazi@users.noreply.github.com>
2023-05-26 19:28:19 +0000

    add render_template

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def Hello_World():
-  return " <p> Hello World </p>"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0' , debug = True)
+  app.run(host='0.0.0.0', debug=True)
